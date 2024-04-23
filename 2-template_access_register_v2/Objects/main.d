8480ae32a4ebcffb8e0acdcfe6277a3a325b40c6
alaaAbdelrahman <a.abdelrahman1399@gmail.com>
2024-04-23 13:07:57 +0200

    V1.1

--- 2-template_access_register_v2/Objects/main.d
+++
@@ -1,5 +0,0 @@
-./objects/main.o: main.cpp mcal_reg.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\cstdint \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
